08af44f6a849d5f581c1edd0ea5b7283152636b6
eslamdyab21 <eslamdyab22@gmail.com>
2023-04-10 05:50:56 +0200

    map known mac addresses (saved in .mac2names.txt) to names - fixes #1

--- Monitor.py
+++ Monitor.py
@@ -20,4 +20,29 @@ class Monitor():
             if 'MAC Address' in line:
                 connected_devices_mac_lst.append(line.split()[2])
 
-        return nmap_original_msg, connected_devices_mac_lst
+        devices_mac_name_dict = self.map_mac_to_names(connected_devices_mac_lst)
+
+        for mac_address, name in devices_mac_name_dict.items():
+            print(mac_address + ' ---> ' + name)
+    
+
+
+    def map_mac_to_names(self, connected_devices_mac_lst):
+        devices_mac_name_dict = {}
+        # record known devices that are in the .mac2names.txt file 
+        with open('.mac2names.txt','r') as file_handler:
+            for line in file_handler.readlines():
+                mac_address, name = line.split()
+                mac_address = mac_address.upper()
+
+                if mac_address in connected_devices_mac_lst:
+                    devices_mac_name_dict[mac_address] = name
+
+
+        # record unknown devices
+        for mac_address in connected_devices_mac_lst:
+            if mac_address not in devices_mac_name_dict.keys():
+                devices_mac_name_dict[mac_address] = '***Unknown***'
+
+
+        return devices_mac_name_dict
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import sys
 from Monitor import Monitor
 
+# ex: python3 main.py 192.168.1.0/24
 monitor = Monitor()
 
-nmap_original_msg, connected_devices_lst = monitor.detect_devices(cider_network=sys.argv[1])
-print(nmap_original_msg)
-print('--------------')
-print(connected_devices_lst)
+monitor.detect_devices(cider_network=sys.argv[1])
